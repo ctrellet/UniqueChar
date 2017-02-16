5deedb8e2233dc152a87e736f2be127ae5eec2e2
ctrellet <christophe@trellet.ch>
2017-02-16 17:49:36 +0100

    Initial commit

---
+++ src/main/java/com/uniquechar/app/UniqueChar.java
@@ -0,0 +1,31 @@
+package com.uniquechar.app;
+
+/**
+ * Created by christophe on 16/02/17.
+ */
+public class UniqueChar {
+
+    public boolean isUniqueChar(String str2check) {
+
+        System.out.println("str2check:"+str2check);
+
+        String existingChars = "";
+        for(String aChar:str2check.split("(?!^)") ){
+
+            System.out.println("aChar:"+aChar);
+
+            if(existingChars.contains(aChar)) {
+                System.out.println("existingChars:"+existingChars+"  contient:"+aChar+" ==> false");
+                return false;
+            }else{
+                System.out.println("existingChars:"+existingChars+"  ne contient pas:"+aChar+" ==> on l'ajoute");
+                existingChars = existingChars + aChar;
+                System.out.println("existingChars:"+existingChars);
+            }
+        }
+        System.out.println("existingChars:"+existingChars+"  fin de boucle ==> true");
+        return true;
+
+    }
+
+}
---
+++ src/test/java/com/uniquechar/app/UniqueCharTest.java
@@ -0,0 +1,29 @@
+package com.uniquechar.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Unit test for UniqueChar.
+ */
+public class UniqueCharTest{
+
+    public UniqueChar uc = new UniqueChar();
+
+    @Test
+    // String of unique chars
+    public void uniqueCharsABCD() {
+        //assert
+        assertTrue(uc.isUniqueChar("ABCD"));
+    }
+
+    @Test
+    // String of unique chars
+    public void noUniqueCharsABAD() {
+        //assert
+        assertTrue(!uc.isUniqueChar("ABAD"));
+    }
+}

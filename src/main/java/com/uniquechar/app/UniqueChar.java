2e7f8a846178edfc9b992c3cc26e189a37abf840
ctrellet <christophe@trellet.ch>
2017-02-20 17:11:01 +0100

    Adding control for null and empty string

--- src/main/java/com/uniquechar/app/UniqueChar.java
+++ src/main/java/com/uniquechar/app/UniqueChar.java
@@ -7,23 +7,27 @@ public class UniqueChar {
 
     public boolean isUniqueChar(String str2check) {
 
-        System.out.println("str2check:"+str2check);
+        if(str2check==null||str2check.isEmpty()){
+            return false;
+        }
+
+        //System.out.println("str2check:"+str2check);
 
         String existingChars = "";
         for(String aChar:str2check.split("(?!^)") ){
 
-            System.out.println("aChar:"+aChar);
+            //System.out.println("aChar:"+aChar);
 
             if(existingChars.contains(aChar)) {
-                System.out.println("existingChars:"+existingChars+"  contient:"+aChar+" ==> false");
+                //System.out.println("existingChars:"+existingChars+"  contient:"+aChar+" ==> false");
                 return false;
             }else{
-                System.out.println("existingChars:"+existingChars+"  ne contient pas:"+aChar+" ==> on l'ajoute");
+                //System.out.println("existingChars:"+existingChars+"  ne contient pas:"+aChar+" ==> on l'ajoute");
                 existingChars = existingChars + aChar;
-                System.out.println("existingChars:"+existingChars);
+                //System.out.println("existingChars:"+existingChars);
             }
         }
-        System.out.println("existingChars:"+existingChars+"  fin de boucle ==> true");
+        //System.out.println("existingChars:"+existingChars+"  fin de boucle ==> true");
         return true;
 
     }
